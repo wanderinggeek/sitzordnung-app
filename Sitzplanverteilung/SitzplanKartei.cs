f3e8ddf2c3c1bfe33bf15690441ba8e1551689a8
VivianSchmidt <vaschmidt@gmx.net>
2017-09-18 12:31:27 +0200

    Methode für Generierungsfunktion mit variabler Anzahl Gruppen und Schüler pro gruppe

--- Sitzplanverteilung/SitzplanKartei.cs
+++ Sitzplanverteilung/SitzplanKartei.cs
@@ -33,6 +33,20 @@ namespace Sitzplanverteilung
             }
         }
 
+        //Sitzplan verteilen mit variablen Gruppengrößen/Anzahl Tischgruppen
+        public void sitzplaeneGenerierenMitDatei(int anzahlGruppen, int maxSchueler)
+        {
+            //Schülerliste importieren
+            List<Schueler> liste = Verwaltungskram.importiereSchuelerListe();
+
+            //Erstellen der einzelnen Sitzpläne
+            for (int i = 0; i < 6; i++)
+            {
+                sitzplan[i] = new Sitzplan(anzahlGruppen, maxSchueler);
+                sitzplan[i].verteileSchueler(liste);
+            }
+        }
+
         public Sitzplan[] getSitzplaene() 
         {
             return this.sitzplan;
