8738fce2f8452bf4468f5a4238e09418745f9e84
Daniel Berg <dab@crosscan.com>
2017-10-02 17:24:17 +0200

    Sitzplaene using real data and can switch blocks

--- Sitzplanverteilung/Views/SitzplanMit2TischenView.xaml.cs
+++ Sitzplanverteilung/Views/SitzplanMit2TischenView.xaml.cs
@@ -24,32 +24,16 @@ namespace Sitzplanverteilung.Views
         public SitzplanMit2TischenView()
         {
             InitializeComponent();
-            DataContext = new SitzplanMit1TischModel();     
+            loadDataForTemplates();
+            DataContext = new SitzplanMit1TischModel();
         }
 
-        // wahrscheinlich brauch ich das hier überhaupt nicht aber ich will es da lassen zur info
-
-        //private void schuelerGenerien()
-        //{
-        //    var numberOfLabels = 6;
-        //    var numberOfTables = 2;
-        //    for (int a = 1; a <= numberOfTables; a++)
-        //    {
-        //        for (int i = 1; i <= numberOfLabels; i++)
-        //        {
-        //            var labelName = string.Format("tisch{0}Platz{1}Name", a, i);
-        //            var labelFirma = string.Format("tisch{0}Platz{1}Firma", a, i);
-        //            var bildName = string.Format("tisch{0}Platz{1}Bild", a, i);
-        //            var labelFuerName = (Label)this.FindName(labelName);
-        //            var labelFuerFirma = (Label)this.FindName(labelFirma);
-        //            var bild = (Image)this.FindName(bildName);
-        //            labelFuerName.Content = "Musterman, Max";
-        //            labelFuerFirma.Content = "Deutsche Bahn";
-
-        //            bild.Source = new BitmapImage(new Uri(@"/Bilder/Cat_Melon.jpg", UriKind.Relative));
-        //        }
-        //    }
-               
-        //}
+        private void loadDataForTemplates()
+        {
+            Sitzplan sitzplanFuerBlock = (Sitzplan)App.Current.Properties["Block"];
+            List<Tischgruppe> aktiveTischegruppen = sitzplanFuerBlock.getTischgruppen();
+            App.Current.Properties["tischNummer"] = 0;
+            App.Current.Properties["Tischgruppe"] = aktiveTischegruppen;
+        }
     }
 }
